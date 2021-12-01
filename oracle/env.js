7222607814674d92546674250b334fb83cf96b7f
eabz <eabz@polispay.org>
2021-12-01 10:38:39 -0600

    Remove codebase

--- oracle/env.js
+++
@@ -1,4 +0,0 @@
-const path = require('path')
-require('dotenv').config({
-  path: path.join(__dirname, process.env.NODE_ENV === 'test' ? './test/test.env' : './.env')
-})
--- oracle/src/events/processAMBInformationRequests/calls/ethBlockNumber.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  'eth_blockNumber()': async (web3, _, block) => [true, web3.eth.abi.encodeParameter('uint256', block.number)]
-}
